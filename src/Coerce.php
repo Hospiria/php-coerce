d91999d1826a40af2ef96ad2c1554bd83544adf3
Dan Howard <dh@madisonsolutions.co.uk>
2018-12-17 14:03:11 +0000

    Initial commit

---
+++ src/Coerce.php
@@ -0,0 +1,207 @@
+<?php
+namespace MadisonSolutions\Coerce;
+
+class Coerce
+{
+    /**
+     * Coerce a value to string type
+     *
+     * Notes:
+     * A null value is coerced to the empty string
+     * Boolean values are coerced to strings 'true' or 'false'
+     * Integers and floats are coerced to their standard string representation
+     * No attempt is made to coerce array inputs to a string - the function will return false
+     * Objects will be coerced if and only if, they have a defined __toString() method
+     * IE the creater of the class has a specific string representation explicity defined
+     *
+     * @param mixed $input The input value
+     * @param mixed &$output If coersion is successful, the string value will be written to the $output variable
+     * @return bool True if $input was successfully coerced to a string, false otherwise
+     */
+    public static function toString($input, &$output) : bool
+    {
+        if (is_null($input)) {
+            $output = '';
+            return true;
+        }
+        if (is_bool($input)) {
+            $output = $input ? 'true' : 'false';
+            return true;
+        }
+        if (is_scalar($input)) {
+            $output = (string) $input;
+            return true;
+        }
+        if (is_object($input) && method_exists($input, '__toString')) {
+            $output = $input->__toString();
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Coerce a value to integer type
+     *
+     * Notes
+     * Null values, arrays and objects will not be coerced - the function will return false
+     * Boolean true will be coerced to 1, and false to 0
+     * If the input value is the float or string representation of an integer, then the value will be coerced,
+     * For example the float 4.0, the string '4.0' or the string '4' would all be coerced to the integer 4
+     * Any representation of a non-integer number will fail coersion - in particular numbers are never rounded to the nearest integer.
+     *
+     * @param mixed $input The input value
+     * @param mixed &$output If coersion is successful, the integer value will be written to the $output variable
+     * @return bool True if $input was successfully coerced to an integer, false otherwise
+     */
+    public static function toInt($input, &$output) : bool
+    {
+        if (is_int($input)) {
+            $output = $input;
+            return true;
+        }
+        if (is_bool($input)) {
+            $output = $input ? 1 : 0;
+            return true;
+        }
+        if (is_string($input) && is_numeric($input)) {
+            $input = (float) $input;
+        }
+        if (is_float($input) && is_finite($input)) {
+            $output = (int) $input;
+            return ( $output == $input );
+        }
+        return false;
+    }
+
+    /**
+     * Coerce a value to float type
+     *
+     * Notes
+     * Null values, arrays and objects will not be coerced - the function will return false
+     * Boolean true will be coerced to 1.0, and false to 0.0
+     * Numeric strings will be coerced to their float values
+     * If the input is technically of float type, but is non-finite (eg NAN or INF),
+     * then the value will not be coerced and the function will return false.
+     *
+     * @param mixed $input The input value
+     * @param mixed &$output If coersion is successful, the float value will be written to the $output variable
+     * @return bool True if $input was successfully coerced to a float, false otherwise
+     */
+    public static function toFloat($input, &$output) : bool
+    {
+        if (is_float($input)) {
+            if (is_finite($input)) {
+                $output = $input;
+                return true;
+            } else {
+                return false;
+            }
+        }
+        if (is_int($input)) {
+            $output = floatval($input);
+            return true;
+        }
+        if (is_bool($input)) {
+            $output = floatval($input ? 1 : 0);
+            return true;
+        }
+        if (is_string($input) && is_numeric($input)) {
+            $output = floatval($input);
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Coerce a value to type suitable for use as a key in a PHP array (string or int)
+     *
+     * Notes:
+     * Null values, arrays and objects will not be coerced - the function will return false.
+     * Boolean values will not be coerced, due to an ambiguity whether to use a string ('true', 'false') or an integer (1, 0) as the key.
+     * Strings or floats representing an integer will be coerced to integer.
+     * Floats representing non-integer values will be converted to strings for use as an array key.
+     * Integers or any string which does not represent an integer will be returned unmodified for use an an array key.
+     *
+     * @param mixed $input The input value
+     * @param mixed &$output If coersion is successful, the coerced string or integer value will be written to the $output variable
+     * @return bool True if $input was successfully coerced to an array key value, false otherwise
+     */
+    public static function toArrayKey($input, &$output) : bool
+    {
+        if (is_null($input) || is_bool($input)) {
+            return false;
+        }
+        if (is_float($input) && ! is_finite($input)) {
+            return false;
+        }
+        if (Coerce::toInt($input, $intval)) {
+            $output = $intval;
+            return true;
+        }
+        if (Coerce::toString($input, $stringval)) {
+            $output = $stringval;
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Coerce a value to boolean type
+     *
+     * Notes:
+     * Null values, arrays and objects will not be coerced - the function will return false
+     * A numeric value (int or float) exactly equal to zero is coerced to boolean false
+     * A numeric value (int or float) exactly equal to one is coerced to boolean true
+     * All other numeric values will not be coerced - the function will return false
+     * The following strings (case-insensitive) will be coerced to boolean true:
+     * '1' 'true' 't' 'yes' 'y' 'on'
+     * The following strings (case-insensitive) will be coerced to boolean false:
+     * '0' 'false' 'f' 'no' 'n' 'off'
+     * All other string values will not be coerced - the function will return false
+     *
+     * @param mixed $input The input value
+     * @param mixed &$output If coersion is successful, the boolean value will be written to the $output variable
+     * @return bool True if $input was successfully coerced to a boolean, false otherwise
+     */
+    public static function toBool($input, &$output) : bool
+    {
+        if (is_bool($input)) {
+            $output = $input;
+            return true;
+        }
+        if (is_int($input) || is_float($input)) {
+            if ($input == 0) {
+                $output = false;
+                return true;
+            } elseif ($input == 1) {
+                $output = true;
+                return true;
+            } else {
+                return false;
+            }
+        }
+        if (is_string($input)) {
+            switch (strtolower($input)) {
+                case '1':
+                case 'true':
+                case 't':
+                case 'yes':
+                case 'y':
+                case 'on':
+                    $output = true;
+                    return true;
+                case '0':
+                case 'false':
+                case 'f':
+                case 'no':
+                case 'n':
+                case 'off':
+                    $output = false;
+                    return true;
+                default:
+                    return false;
+            }
+        }
+        return false;
+    }
+}
---
+++ tests/CoerceTest.php
@@ -0,0 +1,188 @@
+<?php
+
+use MadisonSolutions\Coerce\Coerce;
+use PHPUnit\Framework\TestCase;
+
+class CoerceTest extends TestCase
+{
+    // Helper function which attempts to return helpful information about the
+    // type and value of a variable as concisely as possible for debugging.
+    protected function debugval($val)
+    {
+        if (is_string($val)) {
+            if (strlen($val) > 20) {
+                $val = substr($val, 0, 20) . '...';
+            }
+            return "'{$val}'";
+        } elseif (is_null($val)) {
+            return 'NULL';
+        } elseif (is_bool($val)) {
+            return $val ? 'TRUE' : 'FALSE';
+        } elseif (is_int($val)) {
+            return (string) $val;
+        } elseif (is_float($val)) {
+            if (is_nan($val)) {
+                return 'NAN';
+            } elseif (is_infinite($val)) {
+                return ($val < 0 ? '-INF' : 'INF');
+            } else {
+                return (string) $val;
+            }
+        } elseif (is_array($val)) {
+            $out = [];
+            foreach ($val as $key => $subval) {
+                $out[] = $this->debugval($key) . ': ' . $this->debugval($subval);
+            }
+            $out = implode(', ', $out);
+            if (strlen($out) > 20) {
+                $out = substr($out, 0, 20) . '...';
+            }
+            return '{' . $out . '}';
+        } elseif (is_object($val)) {
+            return get_class($val) . ' ' . (method_exists($val, '__toString') ? $val->__toString() : '-obj-');
+        } else {
+            throw new \Exception("Unexpected type " . gettype($val));
+        }
+    }
+
+    protected function assertCoersionsSucceeds($method, $tests)
+    {
+        foreach ($tests as $test) {
+            $result = Coerce::$method($test[0], $output);
+            $this->assertSame(true, $result, "Could not coerce value " . $this->debugval($test[0]) . " using method {$method}");
+            $this->assertSame($test[1], $output);
+        }
+    }
+
+    protected function assertCoersionsFails($method, $inputs)
+    {
+        foreach ($inputs as $input) {
+            $result = Coerce::$method($input, $output);
+            $this->assertSame(false, $result, "Should not have been able to coerce value " . $this->debugval($input) . " using method {$method} but was coerced to value " . $this->debugval($output));
+        }
+    }
+
+    public function testCoerceToString()
+    {
+        $foo = new class() {
+            public function __toString() {
+                return 'foo';
+            }
+        };
+
+        $this->assertCoersionsSucceeds('toString', [
+            [null, ''],
+            ['', ''],
+            ['foo', 'foo'],
+            [0 ,'0'],
+            [2, '2'],
+            [2.5, '2.5'],
+            [NAN, 'NAN'],
+            [true, 'true'],
+            [false, 'false'],
+            [$foo,  'foo'],
+        ]);
+
+        $this->assertCoersionsFails('toString', [
+            [],
+            new \stdClass(),
+        ]);
+    }
+
+    public function testCoerceToInt()
+    {
+        $this->assertCoersionsSucceeds('toInt', [
+            [0 , 0],
+            [2, 2],
+            [2.0, 2],
+            [true, 1],
+            [false, 0],
+            ['0', 0],
+            ['2.0', 2],
+        ]);
+
+        $this->assertCoersionsFails('toInt', [
+            null,
+            2.5,
+            NAN,
+            INF,
+            '',
+            'foo',
+            '2.5',
+            [],
+            new \stdClass(),
+        ]);
+    }
+
+    public function testCoerceToFloat()
+    {
+        $this->assertCoersionsSucceeds('toFloat', [
+            [0, 0.0],
+            [2, 2.0],
+            [2.0, 2.0],
+            [true, 1.0],
+            [false, 0.0],
+            [2.5, 2.5],
+            ['2.5', 2.5],
+        ]);
+
+        $this->assertCoersionsFails('toFloat', [
+            null,
+            NAN,
+            INF,
+            '',
+            'foo',
+            [],
+            new \stdClass(),
+        ]);
+    }
+
+    public function testCoerceToBool()
+    {
+        $this->assertCoersionsSucceeds('toBool', [
+            [0, false],
+            [1, true],
+            [0.0, false],
+            [1.0, true],
+            [true, true],
+            [false, false],
+            ['t', true],
+            ['TRUE', true],
+            ['no', false],
+        ]);
+
+        $this->assertCoersionsFails('toBool', [
+            null,
+            NAN,
+            INF,
+            '',
+            'foo',
+            '2.5',
+            [],
+            new \stdClass(),
+        ]);
+    }
+
+    public function testCoerceToArrayKey()
+    {
+        $this->assertCoersionsSucceeds('toArrayKey', [
+            [0, 0],
+            ['foo', 'foo'],
+            ['', ''],
+            [1.0, 1],
+            [2.5, '2.5'],
+            ['1', 1],
+            ['1.0', 1],
+            ['2.5', '2.5'],
+        ]);
+
+        $this->assertCoersionsFails('toArrayKey', [
+            null,
+            NAN,
+            INF,
+            true,
+            [],
+            new \stdClass(),
+        ]);
+    }
+}
